f69a5137767f3bea395bdc78474afdf4fb4381e5
Mike Folsom <mike@nissicreative.com>
2015-03-30 15:11:43 -0500

    Initial Commit

---
+++ FormatSource.codaplugin/Contents/Resources/1395D91E-C920-415B-A9F5-09FCEB98AA56/format-source-code.php
@@ -0,0 +1,102 @@
+#!/opt/local/bin/php
+<?php
+//! Requires MacPorts version of PHP compiled with tidy
+
+// Plugin receives a selection or an entire document
+$html = file_get_contents('php://stdin');
+
+
+// If we are processing an entire document, we want to preserve (unaffected) everything up through closing <head> tag
+$head_regex = '~</head>~m';
+
+if (preg_match($head_regex, $html)) {
+	list($head) = preg_split('~</head>~m', $html);
+}
+
+
+// Capture <body> opening tag in case it has an id, class, etc.
+$body_tag = '';
+
+if (preg_match('~<body[^>]*>~', $html, $matches)) {
+	$body_tag = $matches[0];
+}
+
+
+// Remove all line indentations (let Tidy start with a clean slate!)
+$html = preg_replace('~^\s+~m', '', $html);
+
+
+
+//! Tidy markup
+// ================================================== //
+$config = array(
+	'doctype'              => 'auto',
+	'drop-empty-paras'     => 'no',
+	'drop-empty-elements'  => 'no', // for instance, <i class="fa fa-something"></i>
+	'fix-uri'              => 'no',
+	'indent'               => 2, // Must use '2' instead of 'auto' (bug?)
+	'indent-spaces'        => 4,
+	'literal-attributes'   => 'yes', // Otherwise, PHP tags are converted to urlencoded entities
+	'merge-divs'           => 'no',
+	'merge-spans'          => 'no',
+	'new-blocklevel-tags'  => 'article,aside,command,canvas,dialog,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,meter',
+	'new-inline-tags'      => 'video,audio,canvas,ruby,rt,rp,time,meter,progress,datalist,keygen,mark,output,source,wbr',
+	'output-xhtml'          => 'yes',
+	'quote-ampersand'      => 'no',
+	'show-body-only'       => 1,
+	'tab-size'             => 4,
+	'tidy-mark'            => 'no',
+	'vertical-space'       => 'yes',
+	'wrap-php'             => 'no',
+	'wrap-sections'        => 'no',
+	'wrap'                 => 0,
+);
+
+$tidy = new tidy;
+$tidy->parseString($html, $config, 'utf8');
+$tidy->cleanRepair();
+
+
+
+//! Additional post-Tidy cleanup
+// ================================================== //
+
+for ($i = 0; $i < 3; $i++) {
+
+	// Add line breaks before a few other starting tags...
+	$tidy = preg_replace('~^(\s*)(\S+.*)(<(iframe|input|script|!--).*)~m', "$1$2\n$1$3", $tidy);
+
+
+	// Line break before PHP block, unless it is an echo statement
+	$tidy = preg_replace('~(^\s*)(\S+.*)(<\?php(?!.*echo))~m', "$1$2\n$1$3", $tidy);
+
+
+	// Remove line breaks on <script> tags with a 'src' attribute
+	$tidy = preg_replace('~(<script src[^>]*>)(\s*)(</script>)~m', "$1$3", $tidy);
+
+}
+
+// Remove whitespace inside <textarea> tags
+$tidy = preg_replace('~>(\s*)(<\?php(.*)\?>)?\s*(</textarea>)~m', ">$2$4", $tidy);
+
+
+// Four spaces to tab
+$tidy = str_replace('    ', "\t", $tidy);
+
+
+
+//! Output results
+// ================================================== //
+
+if (!empty($head)) {
+	echo $head . "</head>\n";
+	echo "$body_tag\n";
+}
+
+// ($tidy will contain only what's inside the <body> tag of a complete document)
+echo $tidy;
+
+if (!empty($head)) {
+	echo "\n</body>";
+	echo "\n</html>";
+}
